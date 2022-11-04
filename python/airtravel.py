44f38ced529bc3ddb977ab1176ee5edaf4120508
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2022-11-04 14:07:47 +0100

    updates for airtravel.py

--- python/airtravel.py
+++ python/airtravel.py
@@ -18,4 +18,24 @@ class Flight:
     def number(self):
         return self._number
 
+    def airline(self):
+        return self._number[:2]
+
+class Aircraft:
+
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return (range(1, self._num_rows + 1), "ABCDEFGJK"[:self._num_seats_per_row])
+
 
