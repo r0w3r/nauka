fbd135eb85925ae67c11f5380162b71b04e3a32b
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2022-11-09 15:20:27 +0100

    ading perl scripts

---
+++ perl/user_input.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use utf8;
+use strict;
+use warnings;
+
+#testd file and add to open file
+
+my $dirname;
+$dirname=$ARGV[0];
+
+print "Line1\n";
+print "Line2\n";
+print("we will scan for unmasked credi card numbers in this Directory: $dirname \n");
+print "Line3\n";
+print "Line4\n";
+
+
