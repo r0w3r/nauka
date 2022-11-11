343c31717176e596ca056041c824a1770211fbb9
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2022-11-11 13:07:29 +0100

    python new ex

--- python/bmp.py
+++ python/bmp.py
@@ -1,6 +1,6 @@
 """A module for dealing witn bmp bitmap image files."""
 
-def write grayscale(filename, pixels):
+def write_grayscale(filename, pixels):
     """Creates and writes a grayscale BMP file.
 
     Args: 
@@ -29,3 +29,39 @@ def write grayscale(filename, pixels):
         pixel_offset_bookmark = bmp.tell()  #the next four bytes hold the integer offset to the
         bmp.write(b'\x00\x00\x00\x00')      #pixel data. Zero placeholder for now.
 
+        #Image Header
+        bmp.write(b'\x28\x00\x00\x00') # Image header sie in bytes - 40 decimal
+        bmp.write(_int32_to_bytes(width)) # Image width in pixels
+        bmp.write(_int32_to_bytes(height)) # Image height in pixels
+        bmp.write(b'\x01\x00')         # Number of image planes
+        bmp.write(b'\x08\x00')         # Bits per pixel 8 for grayscale
+        bmp.write(b'\x00\x00\x00\x00') # No Compression
+        bmp.write(b'\x00\x00\x00\x00') # Zero for uncompressed images
+        bmp.write(b'\x00\x00\x00\x00') # Unused pixels per meter
+        bmp.write(b'\x00\x00\x00\x00') # Unused pixels per meter
+        bmp.write(b'\x00\x00\x00\x00') # Use whole color table
+        bmp.write(b'\x00\x00\x00\x00') # all colors are important
+
+        # Color palette - a linear grayscale
+        for c in range(256):
+            bmp.write(bytes((c, c, c, 0))) # Blue, Green, Red, Zero
+
+        # Pixel data
+        pixel_data_bookmark = bmp.tell()
+        for row in reversed(pixels): # BMP files are bottom to top
+            row_data = bytes(row)
+            bmp.write(row_data)
+            padding = b'\x00' * ((4 - len(row) % 4)) % 4) # Pad row to miltiple of four bytes
+            bmp.write(padding)
+
+        # end of file
+        eof_bookmark = bmp.tell()
+
+        #Fill in file size placeholder
+        bmp.seek(size_bookmark)
+        bmp.write(_int32_to_bytes(eof_bookmark))
+
+        # Fill in pixel offset placeholder
+        bmp.seek(pixel_offset_bookmark)
+        bmp.write(_int32_to_bytes(pixel_data_bookmark))
+        
