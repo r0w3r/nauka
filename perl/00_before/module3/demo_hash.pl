0649bea9e55773946396dddfc973315de153e724
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2022-11-15 12:19:25 +0100

    perl course next steps mod3hash, module3 finish

--- perl/00_before/module3/demo_hash.pl
+++ perl/00_before/module3/demo_hash.pl
@@ -4,4 +4,14 @@ use utf8;
 use strict;
 use warnings;
 
+my %cards = (
+  card1 => "Visa",
+  card2 => "Master"
+);
+print("Value of first element $cards{card1}", "\n");
+
+my @card_keys = keys %cards;
+print("List of keys: ", @card_keys, "\n");
+$cards{card3} = "Amex";
+print("New element: ", $cards{card3}, "\n");
 
