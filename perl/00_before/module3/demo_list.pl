1bf818180fae63053508e2a9907537ab56ac28a0
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2022-11-15 11:41:00 +0100

    perl course next steps mod3

--- perl/00_before/module3/demo_list.pl
+++ perl/00_before/module3/demo_list.pl
@@ -4,4 +4,15 @@ use utf8;
 use strict;
 use warnings;
 
+my $newcard = "Diners";
+
+print(4,8,12);
+print"\n";
+
+
+print("Visa", "Master", "Amex", "Disvovery","\n");
+print("Visa", "Master", "Amex", "Disvovery",(12,3),"\n");
+print("Visa", "Master", "Amex", "Disvovery",$newcard,(12,3),"\n");
+print qw(Visa Master Amex Disvovery);
+print(("Visa", "Master", "Amex", "Disvovery")[2]);
 
