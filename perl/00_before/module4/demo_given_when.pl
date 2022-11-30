ab4bb5bd596055683022d1074602efc0cc7024d6
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2022-11-30 15:57:19 +0100

    module4 finished, module5 started

--- perl/00_before/module4/demo_given_when.pl
+++ perl/00_before/module4/demo_given_when.pl
@@ -1,4 +1,20 @@
-# Demonstrate the given..when
 #!/usr/bin/perl
+
+use utf8;
 use strict;
 use warnings;
+use feature "switch";
+use experimental "smartmatch";
+
+#test
+my $card = <STDIN>;
+chomp($card);
+$card = lc($card);
+given($card){
+  when('visa'){print("we will chek for visa card in our logs");}
+  when('master'){print("we will chek for master masterin our logs");}
+  when('amex'){print("we will chek for amex card in our logs");}
+  when('discover'){print("we will chek for discover card in our logs");}
+  default{print("This card will not be checked by our utility");}
+}
+   
