8d06f5acb13df0059c1f414e2969ee995def352b
r0w3r <bike123@gmail.com>
2023-01-07 17:14:58 +0100

    sec7

--- perl/sec7_1st.pl
+++ perl/sec7_1st.pl
@@ -4,4 +4,14 @@ use utf8;
 use strict;
 use warnings;
 
+my %my_hash = (
+  "name_01" => "bartosz",
+  "name_02" => "tomasz",
+  "name_03" => "bogdan",
+);
+$my_hash{"name_04"} = "adam";
 
+foreach(values(%my_hash))
+{
+  print("$_\n")
+}
