d98ec6bc9e045959e68ffc31eac59ea7e52cc5bb
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2023-01-13 08:36:12 +0100

    Sec8 another lecture.

--- perl/sec8_1st.pl
+++ perl/sec8_1st.pl
@@ -4,4 +4,12 @@ use utf8;
 use strict;
 use warnings;
 
-
+my $some_var = "Welcome to Perl";
+if($some_var =~ /perl/)
+{
+  print("Perl was found!!\n");
+}
+else
+{
+  print("Perl was not found!!\n");
+}
