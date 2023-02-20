044579ca91b0d575d3c6430a6b93dcc7dd4bd0df
Bartosz Bogdan <bartosz_bogdan@discovery.com>
2023-02-20 14:36:03 +0100

    Terraform lessons

--- terraform/terraform-local-file/main.tf
+++ terraform/terraform-local-file/main.tf
@@ -1,6 +1,11 @@
-resource  "local_sensitive_file" "pet" {
-  filename ="/home/babogdan/nauka/terraform/pets.txt"
-  content = "We love pets!"
-  directory_permission = "0770"
-  file_permission = "0700"
+
+resource "local_file" "pet" {
+  filename = var.filename
+  content = var.file-content["statement2"]
+}
+
+resource "random_pet" "my-pet" {
+  prefix = var.prefix[2]
+  separator = var.separator
+  length = var.length
 }
